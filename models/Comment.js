efdc315aa468af8ba6e1244110904c4726995e98
Magali Pascaud-Topie <magtopie@gmail.com>
2022-06-15 19:58:07 +0200

    comment comment ok

--- models/Comment.js
+++ models/Comment.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const commentSchema = mongoose.Schema({
     userId: { type: String, required: true },
     postId: { type: String, required: true },
-    userImageUrl: { type: String, required: false },
     comment: { type: String, required: true },
     createdDate: { type: Date },
     updatedDate: { type: Date }
