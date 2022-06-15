0b3af66146397e77a74539867c984d00100e0352
Magali Pascaud-Topie <magtopie@gmail.com>
2022-06-15 19:59:05 +0200

    model user ok

--- models/User.js
+++ models/User.js
@@ -8,7 +8,8 @@ const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     userName: { type: String, required: true },
-    userImageUrl: { type: String, required: false }
+    userImageUrl: { type: String, required: false },
+    isAdmin:  { type: Boolean, default: false }
 });
 
 userSchema.plugin(uniqueValidator);
